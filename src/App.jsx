98a9987ceeb940c203fe4b78985d5ccfa138cca6
noaeneyo <noaeneyo@hilma.tech>
2025-11-10 17:48:26 +0200

    posts and comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { BrowserRouter, Link, Route, Routes } from "react-router";
 import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
+import Posts from "./pages/Posts";
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Link to="/Login">Login</Link>
+    <Posts />
+    // <>
+    //   <BrowserRouter>
+    //     <Link to="/Login">Login</Link>
 
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    //     <Routes>
+    //       <Route path="/Login" element={<Login />} />
+    //       <Route path="/Signup" element={<Signup />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </>
   );
 }
 
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,25 +1,46 @@
 import { useState, useEffect } from "react";
-
+import Comments from "../components/Comments";
 function Posts() {
   const [posts, setPosts] = useState([]);
-  const userid = 5;
+  //   const [postsComments, setPostsComments] = useState([]);
+  const [activePostId, setActivePostId] = useState(null);
   useEffect(() => {
-    fetch(`http://localhost:3000/posts/?userId=${userid}`)
+    fetch(`http://localhost:3000/posts`)
       .then((response) => response.json())
       .then((data) => setPosts(data));
   }, []);
+
+  function toggleComments(userid) {
+    if (activePostId === userid) {
+      setActivePostId(null);
+    } else {
+      setActivePostId(userid);
+    }
+  }
 
   return (
     <>
       <div>
         <h1>My posts</h1>
 
-        {posts.map((post) => (
-          <div className="allPosts">
-            <h2 key={post.id}>{post.title} </h2>
-            <h4>{post.body}</h4>
-          </div>
-        ))}
+        {posts.map((post) => {
+          const isCommentsOpen = activePostId === post.id;
+          return (
+            <div className="allPosts" key={post.id}>
+              <h2>{post.title} </h2>
+              <h4>{post.body}</h4>
+
+              <button
+                onClick={() => {
+                  toggleComments(post.id);
+                }}
+              >
+                comments
+              </button>
+              {isCommentsOpen && <Comments postId={post.id} />}
+            </div>
+          );
+        })}
       </div>
     </>
   );
